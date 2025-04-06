0f400aa887226ac172a8bd8639978261c0105322
Magdelene Phua <magdelene_phua@outlook.com>
2025-04-06 17:55:16 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MagdelenePhuaHweeQi-Capstone/',
+  base: '/Mag-Finance-Dashboard/',
   test: {
     globals: true,
     environment: 'jsdom',
